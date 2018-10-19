384217f383374c3c8d855222e1bb513dae5249bd
CosignMyCodesign <dillonwilliamsail@gmail.com>
2018-10-19 10:55:13 -0500

    Finished exercise, no issues or hangups

---
+++ main.js
@@ -0,0 +1,33 @@
+// The learning objective of this exercise is to use the JavaScript if statement inside a loop to do one operation when a certain conditions is true, and a different operation if that condition is false.
+
+// Add logic to addExcitement that places an exclamation point (!) after every third word. Read the following English statement and write the equivalent in JavaSript code to make it work.
+
+// If the current value of the counter variable can be evenly divided by 3 (using the JavaScript remainder operator) add a single exclamation point (!) to the current word in the array.
+
+
+let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+
+function addExcitement (theWordArray) {
+    let buildMeUp = ""
+
+    for (let i = 0; i < theWordArray.length; i++) {
+      /*
+            If the current value of `i` divided by 3 has no
+            remainder, add an exclamation point to the end of
+            the word and then concatenate it to `buildMeUp`.
+
+            Otherwise, just concatenate the word itself.
+         */
+      if (i % 3 === 0 && i > 0) {
+        buildMeUp += `${sentence[i]}! `
+      } else {
+        buildMeUp += `${theWordArray[i]} `;
+      }
+
+        // Print buildMeUp to the console
+        console.log(buildMeUp)
+    }
+
+}
+
+addExcitement(sentence)
